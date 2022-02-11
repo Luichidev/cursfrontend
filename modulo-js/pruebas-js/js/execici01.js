bbef5d4f3a1981608cfc3601a66da3af3dcaa452
luichidev <disconluis@gmail.com>
2022-02-11 13:02:00 +0100

    feat: Add module-js

---
+++ modulo-js/pruebas-js/js/execici01.js
@@ -0,0 +1,37 @@
+// exericici 01
+// 1.-
+let numberOne = 30
+let numberTwo = 11
+
+console.log(
+  `la suma de ${numberOne} y ${numberTwo} es ${numberOne + numberTwo}`
+)
+
+// 2.-
+let msg = "I'm = I am \n You’re = You are"
+// window.alert(msg)23
+
+// 3.-
+// let one = prompt('inserta un Número')
+// console.log('Es número? : ', isNaN(one) ? 'no es número' : 'Si es número')
+
+//4.-
+// let numero1 = prompt('inserta un Número')
+// let numero2 = prompt('inserta otro Número')
+// if (numero1 === numero2) {
+//   console.log('son iguales')
+// } else if (numero1 > numero2) {
+//   console.log(`El ${numero1} es mayor`)
+// } else {
+//   console.log(`El ${numero2} es mayor`)
+// }
+
+// 5.-
+// let numero = prompt('inserta un Número')
+// console.log(numero % 2 === 0 ? 'es par' : 'es impar')
+
+// 6.-
+let altura = prompt('Dime tu altura?')
+let peso = prompt('Dime tu peso?')
+let imc = peso / (altura ^ 2)
+console.log('Tu indice de masa corporal es ' + imc)
