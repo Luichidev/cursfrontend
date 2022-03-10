e71eb558ca072a10e7e14a24abd21d6139b6f3d9
luichidev <disconluis@gmail.com>
2022-03-10 13:53:37 +0100

    feat: Add project s'exercises

--- modulo-js/exercicis/ejercicios-proyecto/index.js
+++ modulo-js/exercicis/ejercicios-proyecto/index.js
@@ -10,5 +10,44 @@
 // DESCRIPTION: Añade producto al cart por el id
 
 function buy(id) {
-  cart.push(products.find((pepito) => pepito.id === id))
+  cartList.push(products.find((producto) => producto.id === id))
+}
+
+// PROTOTYPE: Void cleanCart().
+// DESCRIPTION: Reinicializa el carList
+
+function cleanCart() {
+  cartList.length = 0
+}
+
+// PROTOTYPE: Void subTotal().
+// DESCRIPTION: Calcula el subtotal de cada producto
+
+function subTotal() {
+  const keysSub = Object.keys(subtotal)
+  let cartSub = [],
+    sum = []
+
+  keysSub.forEach((ele) => {
+    cartSub[ele] = cartList.filter((pro) => pro.type === ele)
+  })
+
+  keysSub.forEach((ele) => {
+    sum[ele] = sumTokeys(cartSub[ele], 'price')
+  })
+
+  keysSub.forEach((ele) => {
+    subtotal[ele].value = sum[ele]
+  })
+}
+
+// PROTOTYPE: Array sumTokeys(Array array, String key).
+// DESCRIPTION: Devuelve la suma del contenido de la key de un array
+
+function sumTokeys(array, key) {
+  let total = 0
+  for (let i = 0; i < array.length; i++) {
+    total += array[i][key]
+  }
+  return total
 }
