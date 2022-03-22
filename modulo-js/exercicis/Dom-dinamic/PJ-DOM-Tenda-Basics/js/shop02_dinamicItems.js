abfb2edf9cd56ceeeb9146b16fa7c47c5e59fb92
luichidev <disconluis@gmail.com>
2022-03-22 11:44:56 +0100

    feat: split file functions

--- modulo-js/exercicis/Dom-dinamic/PJ-DOM-Tenda-Basics/js/shop02_dinamicItems.js
+++ modulo-js/exercicis/Dom-dinamic/PJ-DOM-Tenda-Basics/js/shop02_dinamicItems.js
@@ -1,9 +1,34 @@
+let count = 0
+const cards = document.querySelectorAll('.cardProduct')
 
-function addToCart(id) {
-    alert("add to cart")
- }
- 
- 
- function removeFromCart(id) {
-     alert("remove to cart")
- }
+cards.forEach((ele) => {
+  let id = ele.getAttribute('data-id')
+  ele.addEventListener('click', (e) => {
+    if (
+      e.target.className.includes('cardAdd') ||
+      e.target.className.includes('fa-cart-plus')
+    ) {
+      count++
+      addToCart(id)
+      count > 0 ? (notify.innerHTML = count) : false
+    }
+    if (
+      e.target.className.includes('cardRemove') ||
+      e.target.className.includes('cardRemove')
+    ) {
+      count--
+      removeFromCart(id)
+      count > 0 ? (notify.innerHTML = count) : (notify.innerHTML = 0)
+    }
+  })
+})
+
+function addToCart(cardId) {
+  const { name } = products.find(({ id }) => id === +cardId)
+  alert('add to cart => ' + name)
+}
+
+function removeFromCart(cardId) {
+  const { name } = products.find(({ id }) => id === +cardId)
+  alert('remove to cart => ' + name)
+}
--- modulo-js/exercicis/Dom-dinamic/PJ-DOM-Tenda-Basics/js/shop_01_items.js
+++ modulo-js/exercicis/Dom-dinamic/PJ-DOM-Tenda-Basics/js/shop_01_items.js
@@ -1,38 +1,13 @@
 const card1 = document.querySelector('#card1')
 const cardId = card1.getAttribute('data-id')
-let count = 0
+
 //Mode: Static
-// card1.addEventListener('click', (e) => {
-//   if (e.target.className.includes('cardAdd')) {
-//     addToCart(cardId)
-//   } else if (e.target.className.includes('cardRemove')) {
-//     removeFromCart(cardId)
-//   }
-// })
-
-//Mode: Dinamic
-const cards = document.querySelectorAll('.cardProduct')
-
-cards.forEach((ele) => {
-  let id = ele.getAttribute('data-id')
-  ele.addEventListener('click', (e) => {
-    if (
-      e.target.className.includes('cardAdd') ||
-      e.target.className.includes('fa-cart-plus')
-    ) {
-      count++
-      addToCart(id)
-      count > 0 ? (notify.innerHTML = count) : false
-    }
-    if (
-      e.target.className.includes('cardRemove') ||
-      e.target.className.includes('cardRemove')
-    ) {
-      count--
-      removeFromCart(id)
-      count > 0 ? (notify.innerHTML = count) : (notify.innerHTML = 0)
-    }
-  })
+card1.addEventListener('click', (e) => {
+  if (e.target.className.includes('cardAdd')) {
+    addToCart(cardId)
+  } else if (e.target.className.includes('cardRemove')) {
+    removeFromCart(cardId)
+  }
 })
 
 function addToCart(cardId) {
